8a5ebbb3444d95f112303f40904f7295782d749f
MC_DESMOND <macdesmonddikeibeh@gmail.com>
2025-08-11 19:13:07 +0100

    message

--- Message.tsx
+++ Message.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import BaseHOC from "./HOC";
 import { ICssHelper } from "./css";
 import CWind from "./cwind";
-import { dict } from "./anys";
+import { dict, list } from "./anys";
 import { BaseElementProps } from "./ctsx";
 
 interface Ianime {
@@ -12,11 +12,22 @@ interface Ianime {
             after?:ICssHelper
         }
 
+interface IMessageAnimation{
+    on?: boolean,
+    off?: boolean,
+    delay?: number,
+}
+
 export default class MessageHOC{
     protected core:BaseHOC
     protected time:number
     protected effect:string
     protected anime
+    protected animating
+     animations:list<[string,any]> = []
+    protected AnimationClassifier
+    protected isWaiting
+    protected current
     constructor({
         time = 1000, effect = "ease-in-out", 
         anime = {
@@ -29,10 +40,24 @@ export default class MessageHOC{
         this.time = time
         this.effect = effect
         this.anime = anime
+        this.isWaiting = false
+        this.animating = false
+        this.AnimationClassifier = {
+            "on":()=>this.on(),
+            "delay":(time:number)=>this.delay(time),
+            "off":()=>this.off(),
+        }
+        this.current = 0
     }
 
     get Core(){
         return this.core
+    }
+    delay(time:number){
+        this.isWaiting = true
+        setTimeout(() => {
+            this.isWaiting = false
+        }, time);
     }
 
     set Time (val:number){
@@ -46,20 +71,25 @@ export default class MessageHOC{
         this.updateCore()
     }
     get Effect (){return this.effect}
-
+    onOn = ()=>{}
+    onOff = ()=>{}
     on(){
         this.updateCore()
         this.core.Execute(el=>{
+            // console.log(el)
             this.core.style.addStyle(this.anime.before || {})
+            this.onOn()
             setTimeout(()=>{this.core.style.addStyle(this.anime.on || {})},10)
         })
     }
+    
     off(){
         this.updateCore()
         this.core.Execute(el=>{
             this.core.style.addStyle(this.anime.off || {})
             setTimeout(() => {
                 this.core.style.addStyle(this.anime.after || {})
+                this.onOff()
             }, this.time);
         })
     }
@@ -70,6 +100,44 @@ export default class MessageHOC{
         </this.core._>
     }
     Render = this._
+
+    LoopAnimations(animations:list<IMessageAnimation>){
+        this.animations = []
+        this.animating = false
+        for(let animation of animations){
+            if (animation.delay){
+                this.animations.push(["delay",animation.delay])
+            }else if (animation.on){
+                this.animations.push(["on",animation.on])
+            }else if (animation.off){
+                this.animations.push(["off",animation.off])
+            }
+        }
+        this.current = 0
+        this.animating = true
+        // console.log(this.animations)
+        const IntervalAnimate = ()=>{
+            if (!this.isWaiting){
+                this.AnimationClassifier[this.animations[this.current][0] as "on" | "off" | "delay"](this.animations[this.current][1] as any)
+                if (this.animations[this.current][0]!= "delay"){
+                    this.delay(this.time+30)}
+                this.current  = this.current < this.animations.length-1? this.current+=1 : 0  
+            }
+            if (this.animating){
+                requestAnimationFrame(IntervalAnimate)
+            }
+            // console.log(this.current)
+            // console.log(this.core.style.display())
+        }
+        IntervalAnimate()
+    }
+
+    killLoop(){
+        this.animating = false
+        this.animations = []
+        this.current = 0
+        this.isWaiting = false
+    }
 
     updateAnime(Dict = {
             off :{}, 
